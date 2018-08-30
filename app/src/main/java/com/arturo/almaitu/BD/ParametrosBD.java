cc080b72dbcfe91befb478aa234a697fa84171fe
Arturo <r20a@hotmail.com>
2018-08-30 02:44:42 -0500

    kheverga
    basesactualizables

--- app/src/main/java/com/arturo/almaitu/BD/ParametrosBD.java
+++ app/src/main/java/com/arturo/almaitu/BD/ParametrosBD.java
@@ -5,9 +5,10 @@ import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
 public class ParametrosBD extends SQLiteAssetHelper {
     private static final String DATABASE_NAME = "comics.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 3;
 
     public ParametrosBD(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        setForcedUpgrade(DATABASE_VERSION);
     }
 }
